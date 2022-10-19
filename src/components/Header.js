3427546a3139d4b5987892992a7a6efa1a7728eb
Rabishankar Panigrahi <rabishankar.panigrahi14@gmail.com>
2022-10-19 17:43:19 +0530

    hyperlinking header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { Avatar, Button, Stack } from "@mui/material";
 import Box from "@mui/material/Box";
 import React from "react";
 import "./Header.css";
-import { useHistory } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 const Header = ({ children, hasHiddenAuthButtons }) => {
   const logout = () => {
@@ -17,7 +17,7 @@ const Header = ({ children, hasHiddenAuthButtons }) => {
   return (
     <Box className="header">
       <Box className="header-title">
-        <img src="logo_light.svg" alt="QKart-icon"></img>
+        <Link to="/"><img src="logo_light.svg" alt="QKart-icon"></img></Link>
       </Box>
       {children}
       {hasHiddenAuthButtons ? (
